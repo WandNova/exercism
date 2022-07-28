8a03bff0a6cdbb2888b594bcc7eee42a8e77b8ce
WandNova <igor110399@mail.ru>
2022-07-28 11:49:07 +0300

    Complited lesson 10

---
+++ lesson10/readme.md
@@ -0,0 +1,4 @@
+Instructions
+Two-fer or 2-fer is short for two for one. One for you and one for me.
+
+Given a name, return a string with the message:
---
+++ lesson10/two_fer.py
@@ -0,0 +1,9 @@
+""" fill name"""
+
+
+def two_fer(name = "you"):
+    """
+        if two_fer empty then name = you
+    """
+    
+    return f"One for {name}, one for me."
